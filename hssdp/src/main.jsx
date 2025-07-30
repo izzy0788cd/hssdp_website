2e4489603c771a5ae21861e569494a1e32274afc
Ishmael Siddy <ishmael.siddy@outlook.com>
2025-07-30 15:36:13 +1000

    renamed "home.jsx" to "Home.jsx" (Uppercase H)...

--- hssdp/src/main.jsx
+++ hssdp/src/main.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
