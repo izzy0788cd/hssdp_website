e0758b545a223021280203cdaa921f7f087c9e23
Ishmael Siddy <ishmael.siddy@outlook.com>
2025-07-30 14:51:30 +1000

    Fixed the navbar, the links their text, as well as having it sit inside the header component. Also fixed the overflow so that home sits correctly below the header.

--- hssdp/src/components/Navbar.jsx
+++ hssdp/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="bg-[#2e5d30] py-2 px-4 flex justify-center space-x-6 shadow">
+    <nav className="py-2 px-4 flex justify-center space-x-6">
       <Link
         to="/"
         className="text-white text-base font-medium hover:text-blue-200 transition-colors"
--- hssdp/src/main.jsx
+++ hssdp/src/main.jsx
@@ -4,8 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import './index.css'
-import App from './App.jsx'
 import Home from './pages/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
